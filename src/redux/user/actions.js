287ddc22fce7530411432f63a095dc155bf3f27d
Alejandro Molina <alejandromolina@kubesoft.com>
2019-04-17 16:48:55 -0500

    adding action to store the user data in localstorage and dispatch the action to modify the application state

--- src/redux/user/actions.js
+++ src/redux/user/actions.js
@@ -6,16 +6,33 @@ import { UserService } from '../../services/user'
 const Actions = {}
 const LOGIN_NAME = 'LOGIN'
 Actions[LOGIN_NAME] = {
-  ACTIONS: createRequestActions(LOGIN_NAME, {
-    fullfilled: (state, action) => ({ ...state, user: action.payload })
-  }),
-  DISPATCHER: (data) => {
-    return dispatch => requestDispatcher(
-      dispatch,
-      Actions[LOGIN_NAME].ACTIONS,
-      UserService.login(data)
-    )
-  }
+	DISPATCHER: (data) => {
+		return dispatch => requestDispatcher(
+			dispatch,
+			Actions[LOGIN_NAME].ACTIONS,
+			UserService.login(data)
+		)
+	},
+	ACTIONS: createRequestActions(LOGIN_NAME, {
+		fullfilled: (state, action) => ({ ...state, user: action.payload })
+	}),
+}
+
+const SET_USER_LOGGED_NAME = 'SET_USER_LOGGED'
+Actions[SET_USER_LOGGED_NAME] = {
+	DISPATCHER: (user_data) => {
+		return dispatch => UserService.storeUserData(user_data)
+			&& dispatch({
+				type: Actions[SET_USER_LOGGED_NAME].ACTIONS.default.name,
+				payload: user_data
+			})
+	},
+	ACTIONS: {
+		default: {
+			name: SET_USER_LOGGED_NAME,
+			reducer: (state, action) => ({ ...state, user: action.payload })
+		}
+	},
 }
 
 const ActionCreators = extractActionCreators(Actions)
