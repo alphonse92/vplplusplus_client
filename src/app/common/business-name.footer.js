75b9e652fb80580cb3693d4cf0e8e153399f8c74
Alejandro Molina <alejandromolina@kubesoft.com>
2019-04-17 18:52:02 -0500

    adding link to the busines name footer

--- src/app/common/business-name.footer.js
+++ src/app/common/business-name.footer.js
@@ -1,2 +1,8 @@
 import React from 'react'
-export const BusinessNameFooter = ()=>	<p className='center'><small>Universidad Francisco de Paula Santander</small></p>
+export const BusinessNameFooter = () => (
+	<p className='center'>
+		<a href="http://www.ufps.edu.co/" target='_blank' rel="noopener noreferrer">
+			<small>Universidad Francisco de Paula Santander</small>
+		</a>
+	</p>
+)
