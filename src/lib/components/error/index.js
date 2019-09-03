3942401e2f593ac1926c4eab4fd3a20a3c96627b
Eliecer Alejandro Molina Vergel <eliecer.molina@uruit.com>
2019-09-03 10:14:17 -0500

    sho the modal name when is related to in the  modal eerror

--- src/lib/components/error/index.js
+++ src/lib/components/error/index.js
@@ -5,7 +5,7 @@ import { Ok, Dialog } from "../material/modals/input";
 const getErrorTextByType = {
   'resource not found': ({ error }) => error.message,
   'action-cant-perform': ({ error }) => error.message,
-  'validation': ({ error }) => {
+  'validation': ({ error, model }) => {
     return Object
       .keys(error)
       .reduce((acc, validationType) => {
@@ -13,6 +13,7 @@ const getErrorTextByType = {
         const ulWrapper = <ul>{list}</ul>
         const component = (
           <div key={validationType}>
+            <p><strong>Related to: </strong> {model}</p>
             <p><strong>{validationType}</strong></p>
             {ulWrapper}
           </div>
@@ -21,7 +22,7 @@ const getErrorTextByType = {
         return acc.concat([component])
       }, [])
   },
-  'default':()=>'Unknowed error. Please contact VPL plus plus support'
+  'default': () => 'Unknowed error. Please contact VPL plus plus support'
 }
 
 
@@ -31,7 +32,7 @@ export const DialogBroker = ({ error, handleClose }) => {
     open={!!error}
     handleClose={handleClose}
     title={'Something Happened'}
-    text={(getErrorTextByType[error.type] || getErrorTextByType.default)(error)} />
+    text={(getErrorTextByType[error.type] || getErrorTextByType.default)(error)} />
 
   return <></>
 }
