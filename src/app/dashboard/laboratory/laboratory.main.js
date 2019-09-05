463f1a4de4373cb189305be6fdcc6ab4073f72a3
Eliecer Alejandro Molina Vergel <eliecer.molina@uruit.com>
2019-09-05 11:00:45 -0500

    starting to change the title nav bar dinamically

--- src/app/dashboard/laboratory/laboratory.main.js
+++ src/app/dashboard/laboratory/laboratory.main.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Toolbar } from '@material-ui/core';
 import { ProjectTable } from './project/tables/project.table';
 
 export const Main = (props) => {
@@ -9,11 +8,6 @@ export const Main = (props) => {
 
 	return (
 		<React.Fragment>
-			<Toolbar disableGutters>
-				<h1>
-					Virtual Programming Lab Projects
-				</h1>
-			</Toolbar>
 			<ProjectTable onCreateNewProject={onCreateNewProject} onCreateNewProjectFromJson={onCreateNewProjectFromJson} />
 		</React.Fragment>
 	)
