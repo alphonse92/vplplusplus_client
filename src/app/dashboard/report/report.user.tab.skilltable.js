c520f267232c4e6b530762077332ea7c4e09cbfd
Eliecer Alejandro Molina Vergel <eliecer.molina@uruit.com>
2019-09-18 17:02:30 -0500

    add row component to show extra info about a topic

--- src/app/dashboard/report/report.user.tab.skilltable.js
+++ src/app/dashboard/report/report.user.tab.skilltable.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import { SkillMapColors } from '../../../constants';
-import { Tooltip, Collapse } from '@material-ui/core';
+import { Tooltip, Collapse, TableCell } from '@material-ui/core';
 
 export const SkillLevelTag = props => {
   const { skill = 0 } = props
@@ -16,7 +15,7 @@ export const SkillLevelTag = props => {
 }
 
 export class SkillsTable extends React.Component {
-  
+
   state = {
     showMoreInfoMap: {
 
@@ -25,72 +24,78 @@ export class SkillsTable extends React.Component {
 
   showMoreInfo = name => () => {
     const map = this.state.showMoreInfoMap
-    this.setState({ showMoreInfo: { ...map, [name]: !!map[name] } })
+    this.setState({ showMoreInfoMap: { ...map, [name]: !map[name] } })
   }
 
   render() {
     const { skills } = this.props;
     return (
-      <Table >
-        <TableHead>
-          <TableRow>
-            <TableCell>Topic</TableCell>
-            <TableCell>The student know</TableCell>
-            <TableCell>
-              <Tooltip title=" (%) The level shows the student hability of a topic." placement='bottom'>
-                <div>Level*</div>
-              </Tooltip>
-            </TableCell>
-            <TableCell>
-              <Tooltip title="Shows the number of attempts to resolve a test case (unit test)" placement='bottom'>
-                <div>Effort*</div>
-              </Tooltip>
-            </TableCell>
-            <TableCell>
-              <Tooltip title="Test cases approved" placement='bottom'>
-                <div>Approved*</div>
-              </Tooltip>
-
-            </TableCell>
-            <TableCell>
-              <Tooltip title="Test cases not approved" placement='bottom'>
-                <div>Not Approved*</div>
-              </Tooltip>
-            </TableCell>
-            <TableCell><div>Cases</div></TableCell>
-            <TableCell>
-              <Tooltip title="This coefficient has been multiplied to the level to punish a student that never resolved a test case" placement='bottom'>
-                <div>Penalization*</div>
-              </Tooltip>
-            </TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {skills.map((skill, index) => {
-            const { description, name, info: { approved, effort, level, negative_coefficent, not_approved, total } } = skill
-            return (
-              <React.Fragment>
-                <TableRow key={index} onClick={this.showMoreInfo(name)}>
-                  <TableCell >{name}</TableCell>
-                  <TableCell >{description}</TableCell>
-                  <TableCell ><SkillLevelTag skill={level} /></TableCell>
-                  <TableCell >{effort}</TableCell>
-                  <TableCell >{approved}</TableCell>
-                  <TableCell >{not_approved}</TableCell>
-                  <TableCell >{total}</TableCell>
-                  <TableCell >{negative_coefficent.toFixed(2)}</TableCell>
-                </TableRow>
-                <TableRow colSpan="8">
-                  <Collapse style={{ root: { padding: '0px' } }} in={this.state.showMoreInfoMap[name]} timeout="auto" unmountOnExit>
-                    <TableCell colSpan="8">More info</TableCell>
-                  </Collapse>
-                </TableRow>
-              </React.Fragment>
-
-            )
-          })}
-        </TableBody>
-      </Table>
+      <React.Fragment>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Topic</TableCell>
+              <TableCell >The student know</TableCell>
+              <TableCell>
+                <Tooltip title=" (%) The level shows the student hability of a topic." placement='bottom'>
+                  <div>Level*</div>
+                </Tooltip>
+              </TableCell>
+              <TableCell >
+                <Tooltip title="Shows the number of attempts to resolve a test case (unit test)" placement='bottom'>
+                  <div>Effort*</div>
+                </Tooltip>
+              </TableCell>
+              <TableCell >
+                <Tooltip title="Test cases approved" placement='bottom'>
+                  <div>Approved*</div>
+                </Tooltip>
+              </TableCell>
+              <TableCell >
+                <Tooltip title="Test cases not approved" placement='bottom'>
+                  <div>Not Approved*</div>
+                </Tooltip>
+              </TableCell>
+              <TableCell ><div>Cases</div></TableCell>
+              <TableCell >
+                <Tooltip title="This coefficient has been multiplied to the level to punish a student that never resolved a test case" placement='bottom'>
+                  <div>Penalization*</div>
+                </Tooltip>
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {skills.map((skill, index) => {
+              const { description, name, info: { approved, effort, level, negative_coefficent, not_approved, total } } = skill
+              const showMoreInfo = !!this.state.showMoreInfoMap[name]
+              return (
+                <React.Fragment key={name} >
+                  <TableRow onClick={this.showMoreInfo(name)} style={{ cursor: 'pointer' }}>
+                    <TableCell >{name}</TableCell>
+                    <TableCell >{description}</TableCell>
+                    <TableCell ><SkillLevelTag skill={level} /></TableCell>
+                    <TableCell >{effort}</TableCell>
+                    <TableCell >{approved}</TableCell>
+                    <TableCell >{not_approved}</TableCell>
+                    <TableCell >{total}</TableCell>
+                    <TableCell >{negative_coefficent.toFixed(2)}</TableCell>
+                  </TableRow>
+                  {
+                    showMoreInfo &&
+                    <TableRow >
+                      <TableCell colSpan="8">
+                        <Collapse style={{ root: { padding: '0px' } }} in={showMoreInfo} timeout="auto" unmountOnExit>
+                          more info
+                      </Collapse>
+                      </TableCell>
+                    </TableRow>
+                  }
+                </React.Fragment>
+              )
+            })}
+          </TableBody>
+        </Table>
+      </React.Fragment>
     );
   }
 }
