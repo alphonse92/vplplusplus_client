342cb154edf7ea48440a6c4e4f89d280e7681d24
Eliecer Alejandro Molina Vergel <eliecer.molina@uruit.com>
2019-09-18 15:33:23 -0500

    adding to the skills table the ability to show more info about it

--- src/app/dashboard/report/report.project.js
+++ src/app/dashboard/report/report.project.js
@@ -7,7 +7,6 @@ export const ReportProject = (props) => {
 	const { report = [] } = props
 	return (
 		<Flex vertical width="100%">
-			
 			{report.map(r => <UserReportTab key={r._id} report={r} />)}
 		</Flex>
 	)
--- src/app/dashboard/report/report.user.tab.skilltable.js
+++ src/app/dashboard/report/report.user.tab.skilltable.js
@@ -5,7 +5,7 @@ import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import { SkillMapColors } from '../../../constants';
-import { Tooltip } from '@material-ui/core';
+import { Tooltip, Collapse } from '@material-ui/core';
 
 export const SkillLevelTag = props => {
   const { skill = 0 } = props
@@ -15,57 +15,82 @@ export const SkillLevelTag = props => {
   return <span style={{ padding: '3px', backgroundColor: color.color, color: color.text }}>{fixed}</span>
 }
 
-export const SkillsTable = (props) => {
-  const { skills } = props;
-  return (
-    <Table >
-      <TableHead>
-        <TableRow>
-          <TableCell>Topic</TableCell>
-          <TableCell>The student know</TableCell>
-          <TableCell>
-            <Tooltip title=" (%) The level shows the student hability of a topic." placement='bottom'>
-              <div>Level*</div>
-            </Tooltip>
-          </TableCell>
-          <TableCell>
-            <Tooltip title="Shows the number of attempts to resolve a test case (unit test)" placement='bottom'>
-              <div>Effort*</div>
-            </Tooltip>
-          </TableCell>
-          <TableCell>
-            <Tooltip title="Test cases approved" placement='bottom'>
-              <div>Approved*</div>
-            </Tooltip>
+export class SkillsTable extends React.Component {
+  
+  state = {
+    showMoreInfoMap: {
 
-          </TableCell>
-          <TableCell>
-            <Tooltip title="Test cases not approved" placement='bottom'>
-              <div>Not Approved*</div>
-            </Tooltip>
-          </TableCell>
-          <TableCell><div>Cases</div></TableCell>
-          <TableCell>
-            <Tooltip title="This coefficient has been multiplied to the level to punish a student that never resolved a test case" placement='bottom'>
-              <div>Penalization*</div>
-            </Tooltip>
-          </TableCell>
-        </TableRow>
-      </TableHead>
-      <TableBody>
-        {skills.map(({ description, name, info: { approved, effort, level, negative_coefficent, not_approved, total } }, index) => (
-          <TableRow key={index}>
-            <TableCell >{name}</TableCell>
-            <TableCell >{description}</TableCell>
-            <TableCell ><SkillLevelTag skill={level} /></TableCell>
-            <TableCell >{effort}</TableCell>
-            <TableCell >{approved}</TableCell>
-            <TableCell >{not_approved}</TableCell>
-            <TableCell >{total}</TableCell>
-            <TableCell >{negative_coefficent.toFixed(2)}</TableCell>
+    }
+  }
+
+  showMoreInfo = name => () => {
+    const map = this.state.showMoreInfoMap
+    this.setState({ showMoreInfo: { ...map, [name]: !!map[name] } })
+  }
+
+  render(props) {
+    const { skills } = props;
+    return (
+      <Table >
+        <TableHead>
+          <TableRow>
+            <TableCell>Topic</TableCell>
+            <TableCell>The student know</TableCell>
+            <TableCell>
+              <Tooltip title=" (%) The level shows the student hability of a topic." placement='bottom'>
+                <div>Level*</div>
+              </Tooltip>
+            </TableCell>
+            <TableCell>
+              <Tooltip title="Shows the number of attempts to resolve a test case (unit test)" placement='bottom'>
+                <div>Effort*</div>
+              </Tooltip>
+            </TableCell>
+            <TableCell>
+              <Tooltip title="Test cases approved" placement='bottom'>
+                <div>Approved*</div>
+              </Tooltip>
+
+            </TableCell>
+            <TableCell>
+              <Tooltip title="Test cases not approved" placement='bottom'>
+                <div>Not Approved*</div>
+              </Tooltip>
+            </TableCell>
+            <TableCell><div>Cases</div></TableCell>
+            <TableCell>
+              <Tooltip title="This coefficient has been multiplied to the level to punish a student that never resolved a test case" placement='bottom'>
+                <div>Penalization*</div>
+              </Tooltip>
+            </TableCell>
           </TableRow>
-        ))}
-      </TableBody>
-    </Table>
-  );
+        </TableHead>
+        <TableBody>
+          {skills.map((skill, index) => {
+            const { description, name, info: { approved, effort, level, negative_coefficent, not_approved, total } } = skill
+            return (
+              <React.Fragment>
+                <TableRow key={index} onClick={this.showMoreInfo(name)}>
+                  <TableCell >{name}</TableCell>
+                  <TableCell >{description}</TableCell>
+                  <TableCell ><SkillLevelTag skill={level} /></TableCell>
+                  <TableCell >{effort}</TableCell>
+                  <TableCell >{approved}</TableCell>
+                  <TableCell >{not_approved}</TableCell>
+                  <TableCell >{total}</TableCell>
+                  <TableCell >{negative_coefficent.toFixed(2)}</TableCell>
+                </TableRow>
+                <TableRow colSpan="8">
+                  <Collapse style={{ root: { padding: '0px' } }} in={this.state.showMoreInfoMap[name]} timeout="auto" unmountOnExit>
+                    <TableCell colSpan="8">More info</TableCell>
+                  </Collapse>
+                </TableRow>
+              </React.Fragment>
+
+            )
+          })}
+        </TableBody>
+      </Table>
+    );
+  }
 }
