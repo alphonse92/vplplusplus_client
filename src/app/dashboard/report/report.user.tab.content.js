43f3f23c75cd34e9324277b0e09a268f80d1811b
Eliecer Alejandro Molina Vergel <eliecer.molina@uruit.com>
2019-09-18 19:26:36 -0500

    adding open user report button

--- src/app/dashboard/report/report.user.tab.content.js
+++ src/app/dashboard/report/report.user.tab.content.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import {
-  Paper
+  Paper,
+  Button
 } from '@material-ui/core';
+import { FilterList } from '@material-ui/icons';
 import { SkillsTable } from './report.user.tab.skilltable';
-
+import { Flex } from '../../../lib/components/flex';
+import ReportIcon from '@material-ui/icons/AssignmentOutlined';
 export const UserReportTabContent = (props) => {
   const { report } = props
   const { skills } = report
   return (
     <Paper elevation={0}>
+      <Flex horizontal>
+        <Button onClick={console.log}><ReportIcon />Open User Report </Button>
+      </Flex>
       <SkillsTable skills={skills} />
       <p style={{ textAlign: 'center' }}><small>* Means there a usefull tooltip in table header</small></p>
     </Paper >
