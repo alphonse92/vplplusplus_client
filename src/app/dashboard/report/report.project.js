ff6eab8a2c1a9186363d0777ba3957ac535ceff4
Eliecer Alejandro Molina Vergel <eliecer.molina@uruit.com>
2019-09-19 13:06:38 -0500

    adding titles

--- src/app/dashboard/report/report.project.js
+++ src/app/dashboard/report/report.project.js
@@ -2,14 +2,17 @@ import React from 'react';
 import { Flex } from '../../../lib/components/flex';
 import { UserReportTab } from './report.user.tab';
 import { MostSkilledStudentsByTopicCard } from './report.project.skilledstudentbytopic';
+import { Typography } from '@material-ui/core';
 
 export const ReportProject = (props) => {
 	const { report = [] } = props
 	return (
 		<Flex vertical width="100%">
+			<Typography variant="h5" gutterBottom>Stadistics</Typography>
 			<Flex horizontal width="100%">
 				<MostSkilledStudentsByTopicCard report={report} />
 			</Flex>
+			<Typography variant="h5" gutterBottom>Student Reports</Typography>
 			{report.map(r => <UserReportTab key={r._id} report={r} />)}
 		</Flex>
 	)
