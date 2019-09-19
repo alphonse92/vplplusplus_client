04aa9bdc2c09442be00fbc64d6caa5bb533b0016
Eliecer Alejandro Molina Vergel <eliecer.molina@uruit.com>
2019-09-18 19:29:58 -0500

    refactor

--- src/app/dashboard/report/report.user.tab.content.js
+++ src/app/dashboard/report/report.user.tab.content.js
@@ -3,18 +3,14 @@ import {
   Paper,
   Button
 } from '@material-ui/core';
-import { FilterList } from '@material-ui/icons';
 import { SkillsTable } from './report.user.tab.skilltable';
-import { Flex } from '../../../lib/components/flex';
 import ReportIcon from '@material-ui/icons/AssignmentOutlined';
 export const UserReportTabContent = (props) => {
   const { report } = props
   const { skills } = report
   return (
     <Paper elevation={0}>
-      <Flex horizontal>
-        <Button onClick={console.log}><ReportIcon />Open User Report </Button>
-      </Flex>
+      <Button onClick={console.log}><ReportIcon />Open User Report </Button>
       <SkillsTable skills={skills} />
       <p style={{ textAlign: 'center' }}><small>* Means there a usefull tooltip in table header</small></p>
     </Paper >
