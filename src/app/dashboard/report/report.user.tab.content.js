2a6d5f4a82add8b5580bfa4e323882b57c2840c7
Eliecer Alejandro Molina Vergel <eliecer.molina@uruit.com>
2019-09-20 13:16:15 -0500

    fix

--- src/app/dashboard/report/report.user.tab.content.js
+++ src/app/dashboard/report/report.user.tab.content.js
@@ -6,13 +6,13 @@ import {
 import { SkillsTable } from './report.user.tab.skilltable';
 import ReportIcon from '@material-ui/icons/AssignmentOutlined';
 export const UserReportTabContent = (props) => {
-  const { report, showUserReport } = props
+  const { report, showProjects } = props
   const { skills, projects = [] } = report
   return (
     <Paper elevation={0}>
       {props.onOpenUserReport && <Button onClick={props.onOpenUserReport}><ReportIcon />Open User Report </Button>}
       {
-        (showUserReport && projects.length) &&
+        (showProjects && projects.length) &&
         // <ProjectReportTable />
         <p>Should show the student projects ifnormation with links and a lot of stuff</p>
       }
