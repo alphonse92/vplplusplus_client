d7f5daa69f07332f157fc2c0cf2a4c8ceeb99327
Eliecer Alejandro Molina Vergel <eliecer.molina@uruit.com>
2019-09-27 10:50:05 -0500

    finishing the integration to the timeline chart with the endpoint

--- src/app/dashboard/report/report.project.timeline.chart.js
+++ src/app/dashboard/report/report.project.timeline.chart.js
@@ -3,8 +3,64 @@ import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import { ActionCreators } from './redux/actions';
 import { Line } from 'react-chartjs-2';
+import { cutStringAndAddDots } from '../../../lib';
 
-class ProjectReportTimelineChart extends React.Component{
+class ProjectReportTimelineChart extends React.Component {
+  static DATASET_BASE = {
+
+    fill: false,
+    lineTension: 0,
+    backgroundColor: 'rgba(75,192,192,0.4)',
+
+    borderColor: 'rgba(75,192,192,1)',
+    pointBorderColor: 'rgba(75,192,192,1)',
+    pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+    pointHoverBorderColor: 'rgba(220,220,220,1)',
+    pointBackgroundColor: '#fff',
+
+    borderCapStyle: 'butt',
+    borderDash: [],
+    borderDashOffset: 0.0,
+    borderJoinStyle: 'miter',
+    pointBorderWidth: 1,
+    pointHoverRadius: 5,
+    pointHoverBorderWidth: 2,
+    pointRadius: 1,
+    pointHitRadius: 10,
+    // data: [65, 59, 80, 81, 56, 55, 40, 65, 59, 80, 81, 56, 55, 40, 65, 59, 80, 81, 56, 55, 40, 65, 59, 80, 81, 56, 55, 40,]
+  }
+
+  static OPTS_BASE = {
+    scales: {
+      xAxes: [{
+        scaleLabel: {
+          display: true,
+          labelString: 'Time period'
+        }
+      }],
+      yAxes: [{
+        scaleLabel: {
+          display: true,
+          labelString: 'Skill Student Average'
+        }
+      }]
+    },
+    tooltips: {
+      intersect: true,
+      mode: 'index',
+      callbacks: {
+        label: function (tooltipItem, myData) {
+          var label = myData.datasets[tooltipItem.datasetIndex].label || '';
+          if (label) {
+            label += ': ';
+          }
+          label += parseFloat(tooltipItem.value).toFixed(2);
+          return label;
+        }
+      }
+    }
+  }
+
   static mapStateToProps = (state) => {
     const { report: root } = state
     const { project = {} } = root
@@ -26,87 +82,25 @@ class ProjectReportTimelineChart extends React.Component{
   }
 
   render() {
-
-    const chartOpts = {
-      scales: {
-        xAxes: [{
-          scaleLabel: {
-            display: true,
-            labelString: 'Time period'
-          }
-        }],
-        yAxes: [{
-          scaleLabel: {
-            display: true,
-            labelString: 'AVG Skill Achieved'
-          }
-        }]
-      },
-      tooltips: {
-        intersect: false,
-        mode: 'index',
-        callbacks: {
-          label: function (tooltipItem, myData) {
-            var label = myData.datasets[tooltipItem.datasetIndex].label || '';
-            if (label) {
-              label += ': ';
-            }
-            label += parseFloat(tooltipItem.value).toFixed(2);
-            return label;
-          }
-        }
+    const { timeline, options = {} } = this.props
+    const { datasets = []: storeDataSets } = timeline
+    const mostLengthyDataset = storeDataSets.reduce(
+      (mostLength, ds) => ds.lenght >= mostLength ? ds.timeline.length : mostLength,
+      0
+    )
+    const labels = Array.from(Array(mostLengthyDataset), (a, index) => index)
+    const datasets = this.props.datasets.map(ds => {
+      const { project, timeline } = ds
+      const custom = {
+        label: cutStringAndAddDots(project.name),
+        data: timeline.map(({ skill }) => skill)
       }
-    }
+      return { ...ProjectReportTimelineChart.DATASET_BASE, custom }
+    })
 
-    const LineData = {
-      labels: [1, 2, 3, 4, 5, 6, 7, 1, 2, 3, 4, 5, 6, 7, 1, 2, 3, 4, 5, 6, 7, 1, 2, 3, 4, 5, 6, 7, 1, 2, 3, 4, 5, 6, 7,],
-      datasets: [
-        {
-          label: 'My First dataset',
-          fill: false,
-          lineTension: 0,
-          backgroundColor: 'rgba(75,192,192,0.4)',
-          borderColor: 'rgba(75,192,192,1)',
-          borderCapStyle: 'butt',
-          borderDash: [],
-          borderDashOffset: 0.0,
-          borderJoinStyle: 'miter',
-          pointBorderColor: 'rgba(75,192,192,1)',
-          pointBackgroundColor: '#fff',
-          pointBorderWidth: 1,
-          pointHoverRadius: 5,
-          pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-          pointHoverBorderColor: 'rgba(220,220,220,1)',
-          pointHoverBorderWidth: 2,
-          pointRadius: 1,
-          pointHitRadius: 10,
-          data: [65, 59, 80, 81, 56, 55, 40, 65, 59, 80, 81, 56, 55, 40, 65, 59, 80, 81, 56, 55, 40, 65, 59, 80, 81, 56, 55, 40,]
-        },
-        {
-          label: 'My second dataset',
-          fill: false,
-          lineTension: 0.1,
-          backgroundColor: 'rgba(75,192,192,0.4)',
-          borderColor: 'rgba(75,192,192,1)',
-          borderCapStyle: 'butt',
-          borderDash: [],
-          borderDashOffset: 0.0,
-          borderJoinStyle: 'miter',
-          pointBorderColor: 'rgba(75,192,192,1)',
-          pointBackgroundColor: '#fff',
-          pointBorderWidth: 1,
-          pointHoverRadius: 5,
-          pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-          pointHoverBorderColor: 'rgba(220,220,220,1)',
-          pointHoverBorderWidth: 2,
-          pointRadius: 1,
-          pointHitRadius: 10,
-          data: [0, 65, 65, 65, 65, 65, 65, 0, 65, 65, 65, 65, 65, 65, 0, 65, 65, 65, 65, 65, 65, 0, 65, 65, 65, 65, 65, 65,]
-        }
-      ]
-    };
+
     return (
-      <Line data={LineData} options={chartOpts} />
+      <Line data={{ labels, datasets }} options={{ ...ProjectReportTimelineChart.OPTS_BASE, ...options }} />
     )
   }
 }
