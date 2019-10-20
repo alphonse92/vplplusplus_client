24a9e8bc97877cdf120ce30df03f122e6446ad4d
Eliecer Alejandro Molina Vergel <eliecer.molina@uruit.com>
2019-10-20 16:59:20 -0500

    change query parameter name

--- src/services/project.js
+++ src/services/project.js
@@ -93,7 +93,7 @@ class ProjectServiceClass extends WebService {
   }
 
 
-  getReportTimeline(project_id, from, type = 'months', each = 6, steps = 4, topic = [], projects = [], separeByTopic) {
+  getReportTimeline(project_id, from, type = 'months', each = 6, steps = 4, topic = [], project = [], separeByTopic) {
     const query = {
       project_id
       , from
@@ -101,7 +101,7 @@ class ProjectServiceClass extends WebService {
       , each
       , steps
       , topic
-      , projects
+      , project
       , separeByTopic
     }
     const options = { method: 'GET', qs: query }
