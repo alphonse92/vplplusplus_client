859885b7f4a28df40abf71b530c6303249373efe
Eliecer Alejandro Molina Vergel <eliecer.molina@uruit.com>
2019-11-08 17:28:08 -0500

    remove console log

--- src/app/login/login.component.js
+++ src/app/login/login.component.js
@@ -4,12 +4,11 @@ import { MvAuthLogin } from "../../lib/components/auth";
 export const LoginComponent = (props) => {
 	const singleProps = { url: '' }
 	const gcloudProps = true
-
 	const signup = {
 		label: 'Do you haven\'t an account?',
-		url: process.env.URL_REGISTRY
+		url: process.env.REACT_APP_URL_REGISTRY
 	}
-
+	
 	return (
 		<MvAuthLogin
 			onLogin={props.onLogin}
